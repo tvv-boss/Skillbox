023b663edc358eb765244e0f2eff53f94568b857
softcodes <tvv-boss@yandex.ru>
2020-10-27 13:55:03 +1000

    LessonGit1

---
+++ db.py
@@ -0,0 +1,17 @@
+#file = open("filename.txt", "r")
+#cont = file.read()
+#print(cont)
+#file.close()
+#with open("filename.txt", "r") as f:
+f = open("filename.txt", "r")
+#for i in file:
+book = f.readlines()
+dlen = len(book)
+j=0
+for i in book:
+    j+=1
+    if j == dlen:
+        print(str(i[0]) + str(len(i)+1))
+    else:
+        print(str(i[0]) + str(len(i)))
+f.close()
